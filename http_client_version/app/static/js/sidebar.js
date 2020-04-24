3c8a92fa80e3d9ebb29db80ce2e9af2484a1478e
rich <richduchin@gmail.com>
2020-04-24 02:33:31 -0400

    added contextmenu template builder and context menus for chat and serverlist

--- http_client_version/app/static/js/sidebar.js
+++ http_client_version/app/static/js/sidebar.js
@@ -1,4 +1,22 @@
+
+// Selectors
 var $serverList = $('.serverList');
+var $serverIconList = $('#serverIcons');
+//  Modals
+var $discoverModal = $("#discoverModal");
+var $createModal = $("#newServerModal");
+var $leaveModal = $("#leaveConfirmModal");
+var $serverOptionModal = $("serverOptionModal");
+
+// Side Menu Selector for modals
+var $discoverBtn = $("#discoverServer");
+var $createBtn = $("#createServer");
+//temporary help button'
+var $helpBtn = $("#help");
+
+
+
+
 // Hide submenus
 $('#body-row .collapse').collapse('hide');
 
@@ -10,17 +28,6 @@ $('[data-toggle=sidebar-colapse]').click(function () {
     SidebarCollapse();
 });
 
-
-
-
-// $allserver.mouseover(function () {
-//     $(this).mousedown(function (e) {
-//         if (e.which == 3) {
-//             alert($(this).text());
-//             alert($(this).attr('room_id'));
-//         }
-//     });
-// });
 
 
 function SidebarCollapse() {
@@ -38,7 +45,7 @@ function SidebarCollapse() {
     }
 
     // Collapse/Expand icon
-    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+    $('#collapse-icon').toggleClass('fa fa-angle-double-left fas fa-angle-double-right');
 }
 
 socket.on('new server', function (server) {
@@ -55,4 +62,113 @@ socket.on('new server', function (server) {
     $serverList.append($tableCellDiv);
     $tableCellDiv.click();
 
+});
+
+socket.on('server info', function (data) {
+    $serverList.html('');
+    $serverIconList.html('');
+    data.server_list.forEach(function (server) {
+        var $serverIcon = $('<img />').attr("src", server.room_logo_url);
+        var $imgDiv = $('<img />').attr("src", server.room_logo_url)
+            .css({
+                "max-height": "133%",
+                "border-radius": "16%",
+                "margin-right": "10px",
+            });
+        var $serverNameDiv = $('<span class="menu-collapsed"/>')
+            .text(server.room_name);
+        var $tableCellDiv = $('<a />')
+            .addClass('list-group-item your-server list-group-item-action')
+            .attr('room_id', server.room_id)
+            .append($imgDiv, $serverNameDiv);
+        $serverList.append($tableCellDiv);
+
+        $serverIconList.append($('<a/>')
+            .attr({ 'room_id': server.room_id, 'room_name': server.room_name })
+
+            .append($serverIcon));
+    });
+});
+
+
+
+// Handle ServerList Clicks 
+$serverList.on('click', 'a', function () {
+    joinRoom = $(this).text();
+    joinRoomID = $(this).attr('room_id');
+    socket.emit('join server', {
+        "username": username,
+        "roomID": joinRoomID,
+        "room": joinRoom
+    });
+
+});
+
+// Handle serverIcon Clicks 
+
+$serverIconList.on('click', 'a', function () {
+    joinRoom = $(this).text();
+    joinRoomID = $(this).attr('room_id');
+    socket.emit('join server', {
+        "username": username,
+        "roomID": joinRoomID,
+        "room": joinRoom
+    });
+
+});
+
+
+
+$('#serverIcons').on("mouseenter", "a", function () {
+    var yPos = $(this).position().top + $(this).outerHeight() / 2
+    var serverHoverDivHeight = 24
+    var divPosY = yPos - serverHoverDivHeight / 2
+    var $serverHoverDiv = $('<a/>')
+        .addClass('hvr-bubble-left')
+        .attr('id', 'tempHover')
+        .css({
+            "padding": "3px 9px",
+            "position": "absolute",
+            "background-color": "var(--heading-color)",
+            "color": "var(--font-color)",
+            "top": divPosY + "px",
+            "left": "87px",
+            "z-index": 2,
+            "border-radius" :"4px",
+            "box-shadow":"3px 4px 6px 0px rgba(0,0,0,0.75)"
+        }).text($(this).attr('room_name'))
+
+    $('body').append($serverHoverDiv);
+});
+
+$('#serverIcons').on("mouseleave", "a", function () {
+    $('#tempHover').remove();
+
+});
+
+
+
+// When the user clicks the button, open the modal 
+$discoverBtn.on('click', function () {
+    $discoverModal.css('display', "flex");
+    $('#search-heading').addClass('animated fadeIn')
+    $options.addClass('animated slideInDown delay-1s');
+});
+
+//Leave Server Modal AREA ------------
+//Temp button to bring up the modal
+$helpBtn.on('click', function () {
+    $leaveModal.css('display', 'flex');
+});
+
+//When right click on all the servers on the serverList... Do something HERE...
+//Currently Returns the text and room_id of which ever you click on
+//(Think of a way to use the information and pass the arguments)
+$(document).on("contextmenu", ".your-server", function (e) {
+    alert($(this).text());
+    alert($(this).attr('room_id'));
+    $(this).on()
+    $serverOptionModal.css('display', "flex");
+
+    return false;
 });
