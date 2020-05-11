cdd972c6382f2ea19de97edc6a4ab5fee231a06a
Qi <qchen125@gmail.com>
2020-05-11 14:41:48 -0400

    testing  with pymongo

--- MongoDB Test/mongoDB.py
+++ MongoDB Test/mongoDB.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from bson import ObjectId
 
 
 class dbHelper:
@@ -9,14 +9,7 @@ class dbHelper:
 
 
 
-anychatDB = dbHelper()
 
-client = MongoClient("mongodb+srv://anychat:anychatadmin@cluster0-nbbi5.mongodb.net/?retryWrites=true&w=majority")
-db = client.anychatdb
-
-messageCollection = db.messages
-userCollection = db.users
-roomCollection = db.rooms
 
 
 # messageCollection.insert_one(message)
@@ -34,21 +27,43 @@ def DB_add_room(message):
     print(roomID)
 
 #this method updates, update user is deprecated
-def DB_update_user(old, new):
+def DB_replaceall_user(old, new):
     result = userCollection.replace_one(old,new)
     return result
 
-def DB_update_message(old, new):
+def DB_update_user(old, new):
+    result = userCollection.update_one(old,new)
+    return result
+
+def DB_replaceall_message(old, new):
     result = messageCollection.replace_one(old,new)
     return result
 
-def DB_update_room(old, new):
+def DB_replaceall_room(old, new):
     result = roomCollection.replace_one(old,new)
     return result
 
+def print_all_data(cursor):
+    for data in cursor:
+        print(data)
 
-        
 
 
+anychatDB = dbHelper()
 
-message = {'user_id': 2, 'message': 'THIS IS AN ACTUAL TEST', 'timestamp': '2020-05-07 23:02:07.0', 'room_id': 25, 'deleted': 0, 'language': 'pl'}
+client = MongoClient("mongodb+srv://anychat:anychatadmin@cluster0-nbbi5.mongodb.net/?retryWrites=true&w=majority")
+db = client.anychatdb
+messageCollection = db.messages
+userCollection = db.users
+roomCollection = db.rooms
+
+user = {'user_id': 2, 'user': 'faketsunami', 'email': 'new@new.com', 'language': 'pl'}
+
+cursor = userCollection.find()
+print_all_data(cursor)
+print('\n\n\nBEFORE UPDATE\n\n\n')
+# userCollection.insert_one(user)
+result = DB_update_user({'_id': ObjectId('5eb532abff469c11c6397a1b')}, {"$set": { 'email': 'idk@whoknowns.com'}})
+print(result)
+cursor = userCollection.find()
+print_all_data(cursor)
